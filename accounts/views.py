ddf5d20bedafa026b50974d54a958b53f438a7f4
Your Name <you@example.com>
2025-01-27 17:11:29 +0530

    login and logout credentials

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,24 @@
 from django.contrib import messages
 from django.shortcuts import redirect, render
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, auth
 
 # Create your views here.
+def login(request):
+    if request.method ==  'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'invalid')
+            return redirect('login')
+    
+    else:
+        return render(request, 'login.html')
+
 def register(request):
     if request.method == 'POST':
         firstname = request.POST['firstname']
@@ -15,13 +31,21 @@ def register(request):
         if password1 == password2:
             if User.objects.filter(username=username).exists():
                 messages.info(request,"user already exist")
-            elif User.objects.filter(emial=email).exists():
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
                 messages.info(request, "email already taken")
+                return redirect('register')
             else:
                 user = User.objects.create_user(username=username, password=password1, email=email, first_name= firstname, last_name=lastname, )
                 user.save()
-                print('user created')
+                messages.info(request, 'user created')
+                return redirect('login')
         else:
-            print("password not matching")
-        return redirect('/')
+            messages.info(request, "password not matching")
+            return redirect('register')
+        
     return render(request, 'register.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
